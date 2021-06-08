7d67663e2a38396e56589ce6e19eb3d6ccd62ad4
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-06-08 16:50:41 +0300

    news-compontet eklendi- seo çalışıyor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './news/news.component';
 
 
 const routes: Routes = [
-
+  { path: "**", redirectTo: "news", pathMatch: "full" },
+  { path: "news", component: NewsComponent },
 ];
 
 @NgModule({
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { News } from "../models/news";
+import { NewsService } from "../services/news.service";
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from "../models/photo";
+  
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { style } from '@angular/animations';
+import { SeoService } from "../services/seo.service";
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  providers: [NewsService]
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private newsService: NewsService,
+    private activatedRoute:ActivatedRoute,
+    private cdRef: ChangeDetectorRef,
+    private seoService:SeoService,
+   
 
+    ) { }
+
+  news: News[];
+  private mediaSub: Subscription;
+  
   ngOnInit() {
+
+    this.activatedRoute.params.subscribe(params=>{
+     this.seoService.updateTitle('Oldumu')
+      if(params["categoryId"]){
+        this.getNewsForCategories(params["categoryId"])
+      }else{
+        this.getNewsAll()
+      }
+      
+    });
+    
   }
 
+  ngAfterViewInit() {
+    this.cdRef.detectChanges();
+  }
+
+  ngOnDestroy() {
+    if (this.mediaSub) {
+      this.mediaSub.unsubscribe();
+    }
+  }
+
+  getNewsAll() {
+    this.newsService.getNewsAll().subscribe(data=>{
+      this.news = data
+      this.news= this.news.slice().reverse();
+    })   
+  }
+
+  getNewsForCategories(categoryId:number) {
+    this.newsService.getNewsForCategories(categoryId).subscribe(data=>{
+      this.news = data
+      this.news= this.news.slice().reverse();
+    })   
+  }
+
+ 
 }
